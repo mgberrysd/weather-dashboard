bdab93a5139e505dd312f0383e1625d69c375bba
mgberrysd <mgberrysd@gmail.com>
2024-01-12 16:52:40 -0800

    added comments to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
+// variables for lat and lon, will be assigned in function for lat and lon from city
 var lat;
 var lon;
+// sets a default city to search for and populate page with
 var city = 'San Diego';
+// selects element where current forecast will display
 var currentDayDiv = document.querySelector('.current');
+// selects elements where future forecasts will display
 var extendedForcastDiv = document.querySelectorAll('.extended');
+// variable for assigining future forecats to a card
 var cardCount = 0;
 var searchInput = document.querySelector('#citySearchField');
 var submitEl = document.querySelector('form');
+// selects element where the searched cities will display
 var searchedCities = document.querySelector('.searchedCities');
+// variable for saving to and writing from local storage
 var searchedCitiesEl = [];
 
 // API call for current weather data based on lat and lon
@@ -18,10 +25,11 @@ function getCurrentForcast(lattitude, longitude) {
             return response.json();
         })
         .then(function (data) {
+            // clears appended elements if there any exist
             if (currentDayDiv.firstChild) {
                 currentDayDiv.innerHTML = '';
             }
-            // console.log(data)
+            // populates current element based on api response
             var name = document.createElement('h2');
             name.textContent = data.name + ' (' + dayjs().format('dddd MM/DD/YYYY') + ')';
             var icon = document.createElement('img');
@@ -51,13 +59,14 @@ function getExtendedForcast(lattitude, longitude) {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data)
-            // console.log(data.list[0].main)
             for (var i = 0; i < data.list.length; i++) {
+                // clears appended elements if they exist
+                // function will only run if the selected array element is 12 aka noon
                 if (data.list[i].dt_txt.substring(11, 13) == 12) {
                     if (extendedForcastDiv[cardCount].firstChild) {
                         extendedForcastDiv[cardCount].innerHTML = '';
                     }
+                    // populates extended forecast cards based on api response
                     var date = document.createElement('h2');
                     date.textContent = dayjs(data.list[i].dt_txt.substring(0, 10)).format('MM/DD/YYYY');
                     var icon = document.createElement('img');
@@ -68,12 +77,14 @@ function getExtendedForcast(lattitude, longitude) {
                     wind.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
                     var humidity = document.createElement('p');
                     humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + ' %';
+                    // will only populate a specific card based on cardCount
                     extendedForcastDiv[cardCount].append(date);
                     extendedForcastDiv[cardCount].append(icon);
                     extendedForcastDiv[cardCount].append(temp);
                     extendedForcastDiv[cardCount].append(wind);
                     extendedForcastDiv[cardCount].append(humidity);
                     cardCount++;
+                    // resets the card count once it reaches the end so 
                     if (cardCount === 5) {
                         cardCount = 0;
                     }
@@ -93,15 +104,15 @@ function getLatNLon(cityName) {
         .then(function (data) {
             lat = data[0].lat;
             lon = data[0].lon;
-            // console.log(data);
-            // console.log(lat);
-            // console.log(lon);
+// calls the current and extended forecast functions with the returned lat and lon
             getCurrentForcast(lat, lon);
             getExtendedForcast(lat, lon);
         });
 };
 
-
+// event handler for when the submit button is clicked
+// will call the start of the weather API based on the input
+// then will create a new button on page and update local storage
 function handleSearchFormSubmit(event) {
     event.preventDefault();
 
@@ -123,6 +134,8 @@ function handleSearchFormSubmit(event) {
 
 };
 
+// on page load will load local storage and create buttons based on previously searched 
+// will also call the lat and lon function for the default city
 function onLoad() {
     var fromLocal = localStorage.getItem("city");
 
@@ -141,11 +154,14 @@ function onLoad() {
     getLatNLon(city);
 }
 
+// event listener on the searched cities buttons, will call the lat and lon function for the specific city when clicked
 searchedCities.addEventListener('click', function (event) {
     var element = event.target;
     getLatNLon(element.getAttribute('data-city'));
 });
 
+// event listener on the submit button
 submitEl.addEventListener('submit', handleSearchFormSubmit);
 
+// will call the onLoad function to populate the page on load
 onLoad();
